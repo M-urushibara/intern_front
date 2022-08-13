024ade11350543274e80e589226b87610127e275
Mahiro Yoshimochi <mhr.ysmc1360@gmail.com>
2022-08-13 19:54:06 +0900

    add login function

--- intern-app/src/App.tsx
+++ intern-app/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
+        {!isAuthenticated ? (
+          <button onClick={loginWithRedirect}>Log in</button>
+        ) : (
+          <button
+            onClick={() => {
+              logout({ returnTo: window.location.origin });
+            }}
+          >
+            Log out
+          </button>
+        )}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- intern-app/src/index.tsx
+++ intern-app/src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Auth0Provider
+    domain={'dev-4h-of1n3.us.auth0.com'}
+    clientId={'lWMqdYKw5ikHcfzs9c3hW1P9KGZR0kIN'}
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
