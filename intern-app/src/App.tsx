16f8cb3adaa4f0444d1bba41738431e290af15c5
一般入試 <umi_koneko@outlook.jp>
2022-08-28 20:06:33 +0900

    エラーがでる！jsonが読み込めないぽい

--- intern-app/src/App.tsx
+++ intern-app/src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import {Timeline} from './components/timeline'
-import ReactDOM from 'react-dom';
+import React,{ useState, useEffect } from 'react';
+import Post from './components/posts.json'
+import axios from 'axios';
 
 export const App = () => {
-  return (
-    <>
-      <Timeline />
-    </>
-  );
-};
+  const [resultTxt, setResultTxt] = useState("");
+  
+  useEffect(() =>{
+	axios.get("./components/posts.json").then((res)=>{
+    console.log(res);
+    if(res.status !== 200){
+      throw new Error("APIがうまく動作していないようです");
+    }
+    let getstr = res.data.results[0];
 
+    setResultTxt(
+      '${getstr.article_id}\n${getstr.maine_image}'
+    );
+	}).catch((err)=> 
+setResultTxt(`データがうまく取得できませんでした。${err}`)
+);
+})
+
+return(<div>
+  <p>{resultTxt}</p>
+</div>);
+}
+
+//作成時間昇順に並べる
