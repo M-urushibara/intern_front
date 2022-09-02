0529d9433d2d54db7559d69a9ecbeec6d20455aa
Mahiro Yoshimochi <mhr.ysmc1360@gmail.com>
2022-09-02 10:53:36 +0900

    JSONファイルの読み込みと表示ができるようにしたつもり

--- intern-app/src/index.tsx
+++ intern-app/src/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -22,3 +23,11 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+import { render } from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
+>>>>>>> Stashed changes
